c9b344cebe0434839ab6687b754904e9c2d0ef83
Malnati <ricardomalnati@gmail.com>
2020-03-11 20:09:14 -0300

    Removing unrequided logging code.

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-const Tesseract = require('tesseract.js');
-
 const imgPath = './IMG_20200120_184457_.png';
 
-Tesseract.recognize(
-  imgPath,
-  'eng',
-  { logger: m => console.log(m) }
-).then(({ data: { text } }) => {
+console.warn("-----------------------------");
+
+const { createWorker } = require('tesseract.js');
+
+console.log(`Recognizing ${imgPath}`);
+
+const worker = createWorker({
+  logger: m => m,
+});
+
+(async () => {
+  await worker.load();
+  await worker.loadLanguage('eng');
+  await worker.initialize('eng');
+  const { data: { text } } = await worker.recognize(imgPath);
   console.log(text);
-})
+  await worker.terminate();
+})();
