d87247ddfee137c8223b88e5fa0d27bb84c9e74f
Malnati <ricardomalnati@gmail.com>
2020-03-11 17:43:13 -0300

    Finishing POC.

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
-const { createWorker } = require('tesseract.js');
+const Tesseract = require('tesseract.js');
 
-const worker = createWorker();
-const rectangles = [
-  {
-    left: 0,
-    top: 0,
-    width: 500,
-    height: 250,
-  },
-  {
-    left: 500,
-    top: 0,
-    width: 500,
-    height: 250,
-  },
-];
+const imgPath = './IMG_20200120_184457_.png';
 
-(async () => {
-  await worker.load();
-  await worker.loadLanguage('eng');
-  await worker.initialize('eng');
-  const values = [];
-  for (let i = 0; i < rectangles.length; i++) {
-    const { data: { text } } = await worker.recognize('https://tesseract.projectnaptha.com/img/eng_bw.png', { rectangle: rectangles[i] });
-    values.push(text);
-  }
-  console.log(values);
-  await worker.terminate();
-})();
+// const worker = createWorker();
+// const rectangles = [
+//   {
+//     left: 689,
+//     top: 0,
+//     width: 689,
+//     height: 100,
+//   }
+// ];
+
+// (async () => {
+//   await worker.load();
+//   await worker.loadLanguage('eng');
+//   await worker.initialize('eng');
+//   const values = [];
+//   for (let i = 0; i < rectangles.length; i++) {
+//     const { data: { text } } = await worker.recognize(imgPath, { rectangle: rectangles[i] });
+//     values.push(text);
+//   }
+//   console.log(values);
+//   await worker.terminate();
+// })();
+
+Tesseract.recognize(
+  imgPath,
+  'eng',
+  { logger: m => console.log(m) }
+).then(({ data: { text } }) => {
+  console.log(text);
+})
